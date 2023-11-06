b28d12e4ce09f980c4ef15b3c28c07097ab93c12
jguayanes <julianguayanes4@gmail.com>
2023-11-06 18:42:00 -0500

    add images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,22 +4,22 @@ const Main = () => {
   return (
     <section className="flex flex-wrap gap-4 p-2 md:justify-center">
         <div className="text-white border-[5px] rounded-md h-[170px] border-gray-700 w-[150px] grid place-items-center hover:bg-gray-700">
-            <img className="w-16" src="/public/fami.png" alt="" />
+            <img className="w-16" src="/public/images/fami.png" alt="" />
             <h2 className="underline decoration-2 ">familiares y amigos </h2>
             <h2>33/33</h2>
         </div>
         <div className="text-white border-[5px] rounded-md h-[170px] border-gray-700 w-[150px] grid place-items-center hover:bg-gray-700">
-            <img className="w-16" src="/public/abu.png" alt="" />
+            <img className="w-16" src="/public/images/abu.png" alt="" />
             <h2 className="underline decoration-2">Edad y etapas</h2>
             <h2>0/29</h2>
         </div>
         <div className="text-white border-[5px] rounded-md h-[170px] border-gray-700 w-[150px] grid place-items-center hover:bg-gray-700">
-            <img className="w-16" src="/public/cuer.png" alt="" />
+            <img className="w-16" src="/public/images/cuer.png" alt="" />
             <h2 className="underline decoration-2">Partes del cuerpo</h2>
             <h2>0/25</h2>
         </div>
         <div className="text-white border-[5px] rounded-md h-[170px] border-gray-700 w-[150px] grid place-items-center hover:bg-gray-700">
-            <img className="w-16" src="/public/lage.png" alt="" />
+            <img className="w-16" src="/public/images/lage.png" alt="" />
             <h2 className="underline decoration-2">Describiendo</h2>
             <h2>0/46</h2>
         </div>

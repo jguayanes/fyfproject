06a6836da6260e7180a03da665d6b29d413348cf
jguayanes <julianguayanes4@gmail.com>
2023-11-06 18:52:49 -0500

    change url images

--- src/components/Publicity.jsx
+++ src/components/Publicity.jsx
@@ -3,7 +3,7 @@
 const Publicity = () => {
   return (
     <section className="flex justify-center">
-        <img className="w-[300px] rounded-t-2xl md:w-[370px] hover:scale-105" src="/public/images/idiomas-2.jpg" alt="" />
+        <img className="w-[300px] rounded-t-2xl md:w-[370px] hover:scale-105" src="./public/images/idiomas-2.jpg" alt="" />
     </section>
   )
 }
